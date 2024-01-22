095e1d8bd1d4d43c278308cb08d078b5566d721f
Caro639 <120673222+Caro639@users.noreply.github.com>
2024-01-22 13:16:17 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll(".btn");
+const slides = document.querySelectorAll(".slide");
+
+// Tableau d'image : [0, 1, 2, 3]
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const calculNextSlide = e.target.id === "next" ? 1 : -1;
+    const slideActive = document.querySelector(".active");
+
+    newIndex = calculNextSlide + [...slides].indexOf(slideActive);
+
+    if (newIndex < 0) newIndex = [...slides].length - 1;
+
+    if (newIndex >= [...slides].length) newIndex = 0;
+
+    slides[newIndex].classList.add("active");
+
+    slideActive.classList.remove("active");
+  });
+});
